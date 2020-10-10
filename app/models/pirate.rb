cf8c0cf0c244f111cd49abbffd30fc505dd440f4
DSJR1284 <DSJR1284@GMAIL.COM>
2020-10-10 17:45:41 -0400

    Done.

--- app/models/pirate.rb
+++ app/models/pirate.rb
@@ -1,2 +1,17 @@
 class Pirate
+  
+    attr_accessor :name, :weight, :height 
+
+    @@all =[]
+
+    def initialize(params) 
+        @name = params[:name]
+        @weight =  [:weight]
+        @height =  [:height]
+        @@all << self
+    end 
+
+    def self.save 
+        @@all
+    end 
 end
